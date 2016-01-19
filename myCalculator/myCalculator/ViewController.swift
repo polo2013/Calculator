529e2e74821bbe4ab285c00692fbc51089302d82
Zhou Chengbo(周成波) <88503052@cenlydeimac.local>
2016-01-19 18:03:35 +0800

    20160119

--- myCalculator/myCalculator/ViewController.swift
+++ myCalculator/myCalculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber : Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -21,6 +21,63 @@ class ViewController: UIViewController {
         } else {
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation(multiple)
+//        case "÷":
+//            if operateStack.count >= 2 {
+//                displayValue = operateStack.removeLast() / operateStack.removeLast()
+//                enter()
+//            }
+//        case "+":
+//            if operateStack.count >= 2 {
+//                displayValue = operateStack.removeLast() + operateStack.removeLast()
+//                enter()
+//            }
+//        case "−":
+//            if operateStack.count >= 2 {
+//                displayValue = operateStack.removeLast() - operateStack.removeLast()
+//                enter()
+//            }
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double,Double)->Double){
+        if operateStack.count >= 2 {
+            displayValue = operateStack.removeLast() * operateStack.removeLast()
+            enter()
+        }
+    }
+    
+    func multiple (op1:Double,op2:Double)->Double{
+        return op1 * op2
+    }
+    
+    var operateStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operateStack.append(displayValue)
+        print("\(operateStack)")
+        
+    }
+    
+    var displayValue : Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
         }
     }
 
