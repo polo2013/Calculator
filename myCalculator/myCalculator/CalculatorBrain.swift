8d62b8cf65beebf37d64d50b13dcf005c4061942
Zhou Chengbo(周成波) <88503052@cenlydeimac.local>
2016-02-03 16:24:00 +0800

    第四节-01

--- myCalculator/myCalculator/CalculatorBrain.swift
+++ myCalculator/myCalculator/CalculatorBrain.swift
@@ -9,11 +9,16 @@
 import Foundation
 
 class CalculatorBrain {
+    
+    //定义一个枚举类型（1个操作数，1个一元操作，1个二元操作，1个描述字符串）
     private enum Op {
+        //操作数
         case Operand(Double)
+        //一元操作（操作符、计算函数）
         case Operation1(String, Double ->Double)
+        //二元操作（操作符、计算函数）
         case Operation2(String, (Double, Double) ->Double)
-        
+        //这个枚举类型的描述变量
         var description: String {
             get {
                 switch self {
@@ -29,14 +34,19 @@ class CalculatorBrain {
         }
     }
     
+    //操作栈，枚举类型数组
     private var opStack = [Op]()
     
+    //已知操作与枚举类型数组的对应关系，这里是一个字典数组
     private var knowOps = [String : Op]()
     
+    //初始化
     init() {
+        //定义一个函数，输入枚举类型的值，无返回值，作用是给knowOps这个字典数组（数组中的每一个字典，key是字符串，value是枚举类型）赋值
         func learnOp(op: Op) {
             knowOps[op.description] = op
         }
+        //创建实例的时候，就给该实例的knowOps这个字典数组（数组中的每一个字典，key是字符串，value是枚举类型）赋值
         learnOp(Op.Operation2("×", *))
         learnOp(Op.Operation2("÷") { $1 / $0 })
         learnOp(Op.Operation2("+", +))
@@ -44,42 +54,77 @@ class CalculatorBrain {
         learnOp(Op.Operation1("√", sqrt))
     }
     
+    //表示自身，get返回堆栈的描述，set把堆栈重置为newValue
+    var program: AnyObject {
+        get {
+            return opStack.map { $0.description }
+        }
+        
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knowOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    //递归运算，传入枚举类型数组（操作数和操作符），返回操作结果、剩余的栈
     private func evaluate(ops:[Op]) -> (result: Double? , remainingOps: [Op]) {
+        //非空
         if !ops.isEmpty {
             var remainingOps = ops
+            //弹出最后一个元素
             let op = remainingOps.removeLast()
             switch op {
+            //如果弹出的是数字，则返回操作数、堆栈
             case .Operand(let operand):
                 return (operand, remainingOps)
+            //如果是一元操作符
             case .Operation1(_, let operation):
+                //递归，剩余堆栈再取最后一个元素
                 let operandEvaluation = evaluate(remainingOps)
+                //直到取到操作数，才进行运算
                 if let operand = operandEvaluation.result {
                     return (operation(operand), operandEvaluation.remainingOps)
                 }
+            //如果是二元操作符
             case .Operation2(_, let operation):
+                //递归，剩余堆栈再取最后一个元素
                 let op1Evaluation = evaluate(remainingOps)
+                //取到最后一个操作数
                 if let operand1 = op1Evaluation.result {
+                    //递归，剩余堆栈再取最后一个元素
                     let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    //取到倒数第二个操作数
                     if let operand2 = op2Evaluation.result {
                         return (operation(operand1, operand2), op2Evaluation.remainingOps)
                     }
                 }
             }
-            
         }
         return (nil, ops)
     }
     
+    //总运算
     func evaluate() -> Double? {
         let (result, _) = evaluate(opStack)
         return result
     }
     
+    //压入操作数到堆栈，并计算，返回结果
     func pushOperand(operand: Double) -> Double? {
         opStack.append(Op.Operand(operand))
         return evaluate()
     }
     
+    //压入运算符到堆栈，并计算，返回结果
     func performOperation(symbol:String) -> Double? {
         if let operation = knowOps[symbol] {
             opStack.append(operation)
--- myCalculator/myCalculator/ViewController.swift
+++ myCalculator/myCalculator/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //界面上的结果显示窗口
     @IBOutlet weak var display: UILabel!
     
+    //用户是否正在录入
     var userIsInTheMiddleOfTypingANumber = false
     
+    //实例化Model逻辑
     var brain = CalculatorBrain()
 
+    //点击数字：如果正在录入，接着录，如果没有正在录入，则是录入第一个数字
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber {
@@ -26,6 +30,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //点击操作符：如果正在录入，停止录入并计算
     @IBAction func operate(sender: UIButton) {
         if userIsInTheMiddleOfTypingANumber {
             enter()
@@ -39,6 +44,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //停止录入，将窗口里的内容压入，并开始计算，然后显示计算结果
     @IBAction func enter() {
         userIsInTheMiddleOfTypingANumber = false
         if let result = brain.pushOperand(displayValue) {
@@ -49,6 +55,7 @@ class ViewController: UIViewController {
         
     }
     
+    //窗口内容的字符串化
     var displayValue : Double {
         get {
             return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
